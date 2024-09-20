e012838c8ca565578c06085ff19968ac6e812d87
BHuvaneshvar933 <bhuvaneshvar.r.c@gmail.com>
2024-09-21 03:15:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "./components/navbar";
-import HeroSection from "./components/heroSection";
+import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/featureSection";
 import Workflow from "./components/WorkFlow";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <login />
         <FeatureSection />
         <Workflow />
         <Pricing />
